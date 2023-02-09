d0c107899efd38a86daafc82a041e54fbbfb216f
Felix J <felix.jonsson1993@gmail.com>
2023-02-09 15:49:39 +0100

    Added camera rotation

--- TrafficSimulator/Assets/CameraSystem.cs
+++ TrafficSimulator/Assets/CameraSystem.cs
@@ -3,21 +3,29 @@ using UnityEngine.InputSystem;
 
 public class CameraSystem : MonoBehaviour
 {
-    [SerializeField] private float _speed = 50f;
+    [SerializeField] private float _movementSpeed = 50f;
+    [SerializeField] private float _rotationSpeed = 50f;
     private Vector3 _moveDirection;
+    private float _rotateDirection;
 
 
     // Debugging 
     public Vector2 PlayerMovementInput { get; private set; }
-
+    
     private void Update()
     {
-        transform.position += _moveDirection * (_speed * Time.deltaTime);
+        transform.position += _moveDirection * (_movementSpeed * Time.deltaTime);
+        transform.eulerAngles += new Vector3(0, _rotateDirection * _rotationSpeed * Time.deltaTime, 0);
     }
 
     private void OnMove(InputValue value)
     {
         PlayerMovementInput = value.Get<Vector2>();
         _moveDirection = transform.forward * PlayerMovementInput.y + transform.right * PlayerMovementInput.x;
+    }
+
+    private void OnRotate(InputValue value)
+    {
+        _rotateDirection = value.Get<float>();
     }
 }
