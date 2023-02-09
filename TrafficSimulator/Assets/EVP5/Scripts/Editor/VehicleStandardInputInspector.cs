03092646220df80e351e7f89eb9ceae5e0199d02
Martin Blom <martinbl@net.chalmers.se>
2023-02-09 17:57:20 +0100

    Importing assets
    Edy's and European road signs

---
+++ TrafficSimulator/Assets/EVP5/Scripts/Editor/VehicleStandardInputInspector.cs
@@ -0,0 +1,53 @@
+﻿//------------------------------------------------------------------------------------------------
+// Edy's Vehicle Physics
+// (c) Angel Garcia "Edy" - Oviedo, Spain
+// http://www.edy.es
+//------------------------------------------------------------------------------------------------
+
+using UnityEngine;
+using UnityEditor;
+
+namespace EVP
+{
+
+[CustomEditor(typeof(VehicleStandardInput))]
+public class VehicleStandardInputInspector : Editor
+	{
+	public override void OnInspectorGUI ()
+		{
+		InspectorTools.BeginContent();
+		serializedObject.Update();
+
+		EditorGUILayout.PropertyField(serializedObject.FindProperty("target"));
+
+		InspectorTools.SetMinLabelWidth(210);
+		EditorGUILayout.PropertyField(serializedObject.FindProperty("continuousForwardAndReverse"));
+		EditorGUILayout.PropertyField(serializedObject.FindProperty("handbrakeOverridesThrottle"));
+		InspectorTools.SetMinLabelWidth(160);
+
+		SerializedProperty propThrottleAndBrakeInput = serializedObject.FindProperty("throttleAndBrakeInput");
+		EditorGUILayout.PropertyField(propThrottleAndBrakeInput);
+
+		EditorGUILayout.PropertyField(serializedObject.FindProperty("steerAxis"));
+
+		VehicleStandardInput.ThrottleAndBrakeInput throttleAndBrakeInput = (VehicleStandardInput.ThrottleAndBrakeInput)propThrottleAndBrakeInput.enumValueIndex;
+
+		if (throttleAndBrakeInput == VehicleStandardInput.ThrottleAndBrakeInput.SeparateAxes)
+			{
+			EditorGUILayout.PropertyField(serializedObject.FindProperty("throttleAxis"));
+			EditorGUILayout.PropertyField(serializedObject.FindProperty("brakeAxis"));
+			}
+		else
+			{
+			EditorGUILayout.PropertyField(serializedObject.FindProperty("throttleAndBrakeAxis"));
+			}
+
+		EditorGUILayout.PropertyField(serializedObject.FindProperty("handbrakeAxis"));
+		EditorGUILayout.PropertyField(serializedObject.FindProperty("resetVehicleKey"));
+
+		serializedObject.ApplyModifiedProperties();
+		InspectorTools.EndContent();
+		}
+
+	}
+}
