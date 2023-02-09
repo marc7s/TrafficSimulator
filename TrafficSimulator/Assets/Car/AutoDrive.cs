0d78dd167df4645acdf3674e91eb33a65b516adc
Hannes Kaulio <kaulio@student.chalmers.se>
2023-02-09 13:47:25 +0100

    Fixed coding standard and car prefab settings

--- TrafficSimulator/Assets/Car/AutoDrive.cs
+++ TrafficSimulator/Assets/Car/AutoDrive.cs
@@ -17,66 +17,66 @@ namespace Car {
     public class AutoDrive : MonoBehaviour
     {
         [Header("Connections")]
-        public GameObject laneObject;
+        public GameObject LaneObject;
         
         [Header("Settings")]
-        public Mode mode = Mode.Quality;
+        public Mode Mode = Mode.Quality;
         
         [Header("Quality mode settings")]
-        public bool showTargetLine = false;
-        public float maxRepositioningSpeed = 5f;
+        public bool ShowTargetLine = false;
+        public float MaxRepositioningSpeed = 5f;
         [Range(0, 20f)]
-        public float targetLookaheadDistance = 10f;
+        public float TargetLookaheadDistance = 10f;
         
         [Header("Performance mode settings")]
         [Range(0, 100f)]
-        public float speed = 20f;
+        public float Speed = 20f;
 
         // Shared variables
 
         // Quality variables
-        private int targetIndex = 0;
-        private Vector3 target;
-        private WheelController wheelController;
-        private LineRenderer targetLineRenderer;
-        private List<Vector3> lane = new List<Vector3>();
-        private float originalMaxSpeed;
-        private int repositioningTargetIndex = 0;
-        private int repositioningOffset = 1;
-        private Status status = Status.Driving;
+        private int _targetIndex = 0;
+        private Vector3 _target;
+        private WheelController _wheelController;
+        private LineRenderer _targetLineRenderer;
+        private List<Vector3> _lane = new List<Vector3>();
+        private float _originalMaxSpeed;
+        private int _repositioningTargetIndex = 0;
+        private int _repositioningOffset = 1;
+        private Status _status = Status.Driving;
 
         // Performance variables
-        private int positionIndex = 0;
+        private int _positionIndex = 0;
 
         void Start()
         {
             // Get the lane positions
-            LineRenderer line = laneObject.GetComponent<LineRenderer>();
+            LineRenderer line = LaneObject.GetComponent<LineRenderer>();
             Vector3[] positions = new Vector3[line.positionCount];
             line.GetPositions(positions);
-            lane = positions.ToList();
+            _lane = positions.ToList();
 
-            if (mode == Mode.Quality)
+            if (Mode == Mode.Quality)
             {
-                wheelController = GetComponent<WheelController>();
+                _wheelController = GetComponent<WheelController>();
             
                 // Setup target line renderer
                 float targetLineWidth = 0.3f;
-                targetLineRenderer = GetComponent<LineRenderer>();
-                targetLineRenderer.sharedMaterial = new Material(Shader.Find("Standard"));
-                targetLineRenderer.sharedMaterial.SetColor("_Color", Color.red);
-                targetLineRenderer.startWidth = targetLineWidth;
-                targetLineRenderer.endWidth = targetLineWidth;
+                _targetLineRenderer = GetComponent<LineRenderer>();
+                _targetLineRenderer.sharedMaterial = new Material(Shader.Find("Standard"));
+                _targetLineRenderer.sharedMaterial.SetColor("_Color", Color.red);
+                _targetLineRenderer.startWidth = targetLineWidth;
+                _targetLineRenderer.endWidth = targetLineWidth;
 
                 // Save the original max speed
-                originalMaxSpeed = wheelController.GetMaxSpeed();
+                _originalMaxSpeed = _wheelController.GetMaxSpeed();
 
                 // Teleport the vehicle to the start of the lane and set the acceleration to the max
                 Q_TeleportToLane();
-                target = lane[0];
+                _target = _lane[0];
                 Q_SetAccelerationPercent(1f);
             }
-            else if (mode == Mode.Performance)
+            else if (Mode == Mode.Performance)
             {
                 P_MoveToFirstPosition();
             }
@@ -85,16 +85,16 @@ namespace Car {
 
         void Update()
         { 
-            if (mode == Mode.Quality)
+            if (Mode == Mode.Quality)
             {
                 Q_SteerTowardsTarget();
                 Q_UpdateTarget();
-                if (showTargetLine)
+                if (ShowTargetLine)
                 {
                     Q_DrawTargetLine();
                 }
             }
-            else if (mode == Mode.Performance)
+            else if (Mode == Mode.Performance)
             {
                 P_MoveToNextPosition();
             }
@@ -103,7 +103,7 @@ namespace Car {
         void Q_TeleportToLane()
         {
             // Move it to the first position of the lane, offset in the opposite direction of the lane
-            transform.position = lane[0] - 2 * (lane[1] - lane[0]);
+            transform.position = _lane[0] - 2 * (_lane[1] - _lane[0]);
         }
         void Q_SteerTowardsTarget()
         {
@@ -114,7 +114,7 @@ namespace Car {
             float steeringAngle = Vector3.SignedAngle(transform.forward, direction.normalized, Vector3.up) / 90;
 
             // Steer smoothly from the current steering angle to the desired
-            wheelController.SetTurnAnglePercent(Vector3.MoveTowards(new Vector3(wheelController.GetTurnAnglePercent(), 0, 0), new Vector3(steeringAngle, 0, 0), Time.deltaTime).x);
+            _wheelController.SetTurnAnglePercent(Vector3.MoveTowards(new Vector3(_wheelController.GetTurnAnglePercent(), 0, 0), new Vector3(steeringAngle, 0, 0), Time.deltaTime).x);
         }
         void Q_UpdateTarget()
         {
@@ -125,60 +125,60 @@ namespace Car {
             float dot = Vector3.Dot(transform.forward, direction.normalized);
             
             // If the vehicle is driving and the target is behind us and too far away
-            if (status == Status.Driving && dot < 0 && direction.magnitude > targetLookaheadDistance + 1f)
+            if (_status == Status.Driving && dot < 0 && direction.magnitude > TargetLookaheadDistance + 1f)
             {
                 Debug.Log("Repositioning started, slowing down...");
-                status = Status.RepositioningInitiated;
+                _status = Status.RepositioningInitiated;
 
                 // Reposition to the point prior to the one we missed
-                repositioningTargetIndex = (targetIndex - repositioningOffset) % lane.Count;
+                _repositioningTargetIndex = (_targetIndex - _repositioningOffset) % _lane.Count;
 
                 // Slow down and limit the max speed to the repositioning speed or 30% of the max speed, whichever is lower
-                wheelController.SetBrakingForcePercent(1);
-                wheelController.SetMaxSpeed(Math.Min(wheelController.GetMaxSpeed() * 0.3f, maxRepositioningSpeed));
+                _wheelController.SetBrakingForcePercent(1);
+                _wheelController.SetMaxSpeed(Math.Min(_wheelController.GetMaxSpeed() * 0.3f, MaxRepositioningSpeed));
             }
             // If the vehicle has started repositioning and slowed down enough
-            else if (status == Status.RepositioningInitiated && wheelController.GetCurrentSpeed() <= wheelController.GetMaxSpeed())
+            else if (_status == Status.RepositioningInitiated && _wheelController.GetCurrentSpeed() <= _wheelController.GetMaxSpeed())
             {
                 Debug.Log("Slowed down, releasing brakes and repositioning...");
-                status = Status.Repositioning;
+                _status = Status.Repositioning;
 
-                wheelController.SetBrakingForcePercent(0);
+                _wheelController.SetBrakingForcePercent(0);
             }
             // If the vehicle is currently repositioning
-            else if (status == Status.Repositioning) 
+            else if (_status == Status.Repositioning) 
             {
                 // Allow the vehicle to accelerate and reverse, whatever takes it to the target faster.
                 // It will accelerate if the target is in front, and reverse if it's behind
-                wheelController.SetAccelerationPercent(wheelController.GetMaxAcceleration() * (dot > 0 ? 1 : -1));
+                _wheelController.SetAccelerationPercent(_wheelController.GetMaxAcceleration() * (dot > 0 ? 1 : -1));
 
                 // If the target is in front of us and we are close enough we have successfully repositioned
-                if (dot > 0 && direction.magnitude <= targetLookaheadDistance - 1f) 
+                if (dot > 0 && direction.magnitude <= TargetLookaheadDistance - 1f) 
                 {
                     Debug.Log("Repositioned, speeding back up...");
-                    status = Status.Driving;
+                    _status = Status.Driving;
                     
                     // Set the target to the one after the target we missed
-                    targetIndex = (repositioningTargetIndex + repositioningOffset + 1) % lane.Count;
-                    target = lane[targetIndex];
+                    _targetIndex = (_repositioningTargetIndex + _repositioningOffset + 1) % _lane.Count;
+                    _target = _lane[_targetIndex];
                     
                     // Reset the max speed to the original, and set the acceleration to the max again
-                    wheelController.SetMaxSpeed(originalMaxSpeed);
-                    wheelController.SetAccelerationPercent(wheelController.GetMaxAcceleration());
+                    _wheelController.SetMaxSpeed(_originalMaxSpeed);
+                    _wheelController.SetAccelerationPercent(_wheelController.GetMaxAcceleration());
                 }
             }
             // If the vehicle is driving and the target is in front of us and we are close enough
-            else if (status == Status.Driving && dot > 0 && direction.magnitude <= targetLookaheadDistance)
+            else if (_status == Status.Driving && dot > 0 && direction.magnitude <= TargetLookaheadDistance)
             {
                 // Set the target to the next point in the lane
-                targetIndex = (targetIndex + 1) % lane.Count;
-                target = lane[targetIndex];
+                _targetIndex = (_targetIndex + 1) % _lane.Count;
+                _target = _lane[_targetIndex];
             }
         }
 
         Vector3 Q_GetTarget()
         {
-            return (status == Status.Driving ? target : lane[repositioningTargetIndex]);
+            return (_status == Status.Driving ? _target : _lane[_repositioningTargetIndex]);
         }
 
         void Q_SetAccelerationPercent(float accelerationPercent) 
@@ -189,26 +189,26 @@ namespace Car {
         
         void Q_DrawTargetLine()
         {
-            targetLineRenderer.SetPositions(new Vector3[] { transform.position, Q_GetTarget() });
+            _targetLineRenderer.SetPositions(new Vector3[] { transform.position, Q_GetTarget() });
         }
 
         // Performance methods
         void P_MoveToFirstPosition()
         {
             // Move to the first position of the lane
-            transform.position = lane[0];
+            transform.position = _lane[0];
         }
 
         void P_MoveToNextPosition()
         {
-            Vector3 target = P_GetLerpPosition(lane[positionIndex]);
+            Vector3 target = P_GetLerpPosition(_lane[_positionIndex]);
             
             if(transform.position == target) 
             {
-                positionIndex++;
-                if(positionIndex == lane.Count) 
+                _positionIndex++;
+                if(_positionIndex == _lane.Count) 
                 {
-                    positionIndex = 0;
+                    _positionIndex = 0;
                     P_MoveToFirstPosition();
                     return;
                 }
@@ -218,7 +218,7 @@ namespace Car {
 
         private Vector3 P_GetLerpPosition(Vector3 target)
         {
-            return Vector3.MoveTowards(transform.position, target, speed * Time.deltaTime);
+            return Vector3.MoveTowards(transform.position, target, Speed * Time.deltaTime);
         }
     }
 }
