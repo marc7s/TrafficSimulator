61681a5d23ffbf0ea58137b5e921950a74281eec
Felix J <felix.jonsson1993@gmail.com>
2023-02-09 17:07:14 +0100

    Add option to pan camera with mouse

--- TrafficSimulator/Assets/CameraSystem.cs
+++ TrafficSimulator/Assets/CameraSystem.cs
@@ -8,10 +8,13 @@ public class CameraSystem : MonoBehaviour
     private Vector3 _moveDirection;
     private float _rotateDirection;
 
+    // Should be set by a function of the current screen size
+    private int _edgeScrollSize = 20;
 
     // Debugging 
-    public Vector2 PlayerMovementInput { get; private set; }
-    
+    private Vector2 _playerMovementInput;
+    private Vector2 _playerPointInput;
+
     private void Update()
     {
         transform.position += _moveDirection * (_movementSpeed * Time.deltaTime);
@@ -20,12 +23,43 @@ public class CameraSystem : MonoBehaviour
 
     private void OnMove(InputValue value)
     {
-        PlayerMovementInput = value.Get<Vector2>();
-        _moveDirection = transform.forward * PlayerMovementInput.y + transform.right * PlayerMovementInput.x;
+        _playerMovementInput = value.Get<Vector2>();
+        _moveDirection = TranslateDirectionToForward(_playerMovementInput.y, _playerMovementInput.x);
     }
 
     private void OnRotate(InputValue value)
     {
         _rotateDirection = value.Get<float>();
+    }
+
+    private void OnPoint(InputValue value)
+    {
+        _playerPointInput = value.Get<Vector2>();
+        float forward = 0;
+        float sideways = 0;
+        if (_playerPointInput.y < _edgeScrollSize)
+        {
+            forward = -1f;
+        }
+        else if (_playerPointInput.y > Screen.height - _edgeScrollSize)
+        {
+            forward = 1f;
+        }
+
+        if (_playerPointInput.x < _edgeScrollSize)
+        {
+            sideways = -1f;
+        }
+        else if (_playerPointInput.x > Screen.width - _edgeScrollSize)
+        {
+            sideways = 1f;
+        }
+
+        _moveDirection = TranslateDirectionToForward(forward, sideways);
+    }
+
+    private Vector3 TranslateDirectionToForward(float forward, float sideways)
+    {
+        return transform.forward * forward + transform.right * sideways;
     }
 }
