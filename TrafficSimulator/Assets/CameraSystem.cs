56dfaa4c3732dc830175c73a68dcf40ed5b195cc
Felix J <felix.jonsson1993@gmail.com>
2023-02-10 10:54:59 +0100

    Minor Refactoring

--- TrafficSimulator/Assets/CameraSystem.cs
+++ TrafficSimulator/Assets/CameraSystem.cs
@@ -3,21 +3,31 @@ using UnityEngine.InputSystem;
 
 public class CameraSystem : MonoBehaviour
 {
+    [SerializeField] private bool _enableEdgeScrolling;
     [SerializeField] private float _movementSpeed = 50f;
     [SerializeField] private float _rotationSpeed = 50f;
-    private Vector3 _moveDirection;
-    private float _rotateDirection;
 
     // Should be set by a function of the current screen size
-    private int _edgeScrollSize = 20;
-
-    // Debugging 
+    private readonly int _edgeScrollSize = 20;
+    private Vector3 _moveDirection;
+    
     private Vector2 _playerMovementInput;
     private Vector2 _playerPointInput;
+    private float _rotateDirection;
 
     private void Update()
     {
+        HandleMovement();
+        HandleRotation();
+    }
+
+    private void HandleMovement()
+    {
         transform.position += _moveDirection * (_movementSpeed * Time.deltaTime);
+    }
+
+    private void HandleRotation()
+    {
         transform.eulerAngles += new Vector3(0, _rotateDirection * _rotationSpeed * Time.deltaTime, 0);
     }
 
@@ -34,32 +44,24 @@ public class CameraSystem : MonoBehaviour
 
     private void OnPoint(InputValue value)
     {
+        if (!_enableEdgeScrolling) return;
+
         _playerPointInput = value.Get<Vector2>();
         float forward = 0;
         float sideways = 0;
         if (_playerPointInput.y < _edgeScrollSize)
-        {
             forward = -1f;
-        }
-        else if (_playerPointInput.y > Screen.height - _edgeScrollSize)
-        {
-            forward = 1f;
-        }
+        else if (_playerPointInput.y > Screen.height - _edgeScrollSize) forward = 1f;
 
         if (_playerPointInput.x < _edgeScrollSize)
-        {
             sideways = -1f;
-        }
-        else if (_playerPointInput.x > Screen.width - _edgeScrollSize)
-        {
-            sideways = 1f;
-        }
+        else if (_playerPointInput.x > Screen.width - _edgeScrollSize) sideways = 1f;
 
         _moveDirection = TranslateDirectionToForward(forward, sideways);
     }
 
-    private Vector3 TranslateDirectionToForward(float forward, float sideways)
+    private Vector3 TranslateDirectionToForward(float forwardScalar, float sidewaysScalar)
     {
-        return transform.forward * forward + transform.right * sideways;
+        return transform.forward * forwardScalar + transform.right * sidewaysScalar;
     }
 }
