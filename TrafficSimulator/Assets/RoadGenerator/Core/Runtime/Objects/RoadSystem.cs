7ea90535ac63be5b86cad37cc2eb0f938280b2bb
marc7s <marcus.schagerberg@gmail.com>
2023-02-17 15:37:49 +0100

    Connected intersection prefab

--- TrafficSimulator/Assets/RoadGenerator/Core/Runtime/Objects/RoadSystem.cs
+++ TrafficSimulator/Assets/RoadGenerator/Core/Runtime/Objects/RoadSystem.cs
@@ -77,7 +77,8 @@ namespace RoadGenerator
             }
         }
 
-        public Intersection AddNewIntersection(IntersectionPointData intersectionPointData, Road road1, Road road2){
+        public Intersection AddNewIntersection(IntersectionPointData intersectionPointData, Road road1, Road road2)
+        {
             Vector3 intersectionPosition = new Vector3(intersectionPointData.Position.x, 0, intersectionPointData.Position.y);
             GameObject intersectionObject = Instantiate(_intersectionPrefab, intersectionPosition, intersectionPointData.Rotation);
             intersectionObject.name = "Intersection" + IntersectionCount;
