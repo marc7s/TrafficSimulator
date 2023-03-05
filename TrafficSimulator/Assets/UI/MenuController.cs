e58da6e44828b48ed2f2cdc624acbbfb4e485775
Hannes Kaulio <kaulio@student.chalmers.se>
2023-03-05 17:37:47 +0100

    Added settings buttons

--- TrafficSimulator/Assets/UI/MenuController.cs
+++ TrafficSimulator/Assets/UI/MenuController.cs
@@ -9,43 +9,62 @@ public class MenuController : MonoBehaviour
     private Button _startButton;
     private Button _settingsButton;
     private Button _exitButton;
-    private Button _settingsBackButton;
 
+    //  Settings UI
+    private Button _settingsBackButton;
+    private Toggle _FPSToggle;
+
+    private Toggle _FullScreenToggle;
+    private DropdownField _GraphicsQualityDropdown;
+    private Slider _MasterVolumeSlider;
+    private Slider _FOVSlider;
     private Label _fpsLabel;
 
-    private Toggle _FPSToggle;
+
     void Awake()
     {
         _doc = GetComponent<UIDocument>();
         _buttonWrapper = _doc.rootVisualElement.Q<VisualElement>("Buttons");
+
+        // Start Menu UI
         _startButton = _doc.rootVisualElement.Q<Button>("StartButton");
-        _startButton.clicked += PlayButtonOnClicked;
+        _startButton.clicked += StartButtonOnClicked;
         _settingsButton = _doc.rootVisualElement.Q<Button>("SettingsButton");
         _settingsButton.clicked += SettingsButtonOnClicked;
         _exitButton = _doc.rootVisualElement.Q<Button>("ExitButton");
         _exitButton.clicked += ExitButtonOnClicked;
-        _fpsLabel = _doc.rootVisualElement.Q<Label>("fpsLabel");
+        _fpsLabel = _doc.rootVisualElement.Q<Label>("FPSLabel");
 
-
+        // Settings UI
         _settingsButtons = _settingsUI.CloneTree();
         _settingsBackButton = _settingsButtons.Q<Button>("BackButton");
         _settingsBackButton.clicked += SettingsBackButtonOnClicked;
+        _FPSToggle = _settingsButtons.Q<Toggle>("FPSToggle");
+        _FPSToggle.RegisterValueChangedCallback(evt => OnFPSToggleChanged(evt.newValue));
+        _FullScreenToggle = _settingsButtons.Q<Toggle>("FullscreenToggle");
+        _FullScreenToggle.RegisterValueChangedCallback(evt => OnFullScreenToggleChanged(evt.newValue));
+        _GraphicsQualityDropdown = _settingsButtons.Q<DropdownField>("GraphicsQualityDropdown");
+        _GraphicsQualityDropdown.RegisterValueChangedCallback(evt => OnGraphicsQualityDropdownChanged(evt.newValue));
+        _MasterVolumeSlider = _settingsButtons.Q<Slider>("MasterVolumeSlider");
+        _MasterVolumeSlider.RegisterValueChangedCallback(evt => OnMasterVolumeSliderChanged(evt.newValue));
+        _FOVSlider = _settingsButtons.Q<Slider>("FOVSlider");
+        _FOVSlider.RegisterValueChangedCallback(evt => OnFOVSliderChanged(evt.newValue));
+        _FOVSlider = _settingsButtons.Q<Slider>("FOVSlider");
+
         
     }
-    private void PlayButtonOnClicked()
+    private void StartButtonOnClicked()
     {
         // TODO switch to game scene
-        Debug.Log("Play");
+        Debug.Log("Start");
     }
 
     private void SettingsButtonOnClicked()
     {
         _buttonWrapper.Clear();
         _buttonWrapper.Add(_settingsButtons);
-        _FPSToggle = _doc.rootVisualElement.Q<Toggle>("FPSToggle");
         _FPSToggle.value = PlayerPrefs.GetInt("FPSCounter", 0) == 1;
-        _FPSToggle.RegisterValueChangedCallback(evt => OnFPSToggleChanged(evt.newValue));
-        Debug.Log(_FPSToggle);
+        _FullScreenToggle.value = Screen.fullScreen;
     }
 
     private void ExitButtonOnClicked()
@@ -59,6 +78,26 @@ public class MenuController : MonoBehaviour
         _fpsLabel.visible = value;
     }
 
+    private void OnFullScreenToggleChanged(bool value)
+    {
+        PlayerPrefs.SetInt("FullScreen", value ? 1 : 0);
+        Screen.fullScreen = value;
+    }
+    private void OnGraphicsQualityDropdownChanged(string value)
+    {
+        //TODO implement quality settings
+    }
+
+    private void OnMasterVolumeSliderChanged(float value)
+    {
+        //TODO implement volume settings
+    }
+
+    private void OnFOVSliderChanged(float value)
+    {
+        //TODO implement FOV settings
+    }
+
     private void SettingsBackButtonOnClicked()
     {
         _buttonWrapper.Clear();
@@ -69,13 +108,10 @@ public class MenuController : MonoBehaviour
 
     void Update()
     {
+        // FPS counter
         if (PlayerPrefs.GetInt("FPSCounter", 0) == 1)
-        {
             _fpsLabel.text = "FPS: " + (1f / Time.unscaledDeltaTime).ToString("F0");
-        }
         else
-        {
             _fpsLabel.text = "";
-        }
     }
 }
