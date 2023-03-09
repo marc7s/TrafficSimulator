31492549eae4ccdfab5ac79bdc9d5373f9c1e7e3
Jakob Windt <jakob.windt@gmail.com>
2023-03-09 14:35:14 +0100

    Added Camera buttons

--- TrafficSimulator/Assets/UI/OverlayController.cs
+++ TrafficSimulator/Assets/UI/OverlayController.cs
@@ -7,8 +7,11 @@ public class OverlayController : MonoBehaviour
     private MenuController _menuController;
     
     // Overlay UI 
-    private VisualElement _settingsContainer;
     private Button _menuButton;
+    private Button _freeCamButton;
+    private Button _twoDButton;
+    private Button _fpvButton;
+    private Label _clockLabel;
 
 
     private const string FULLSCREEN = "Fullscreen";
@@ -19,18 +22,51 @@ public class OverlayController : MonoBehaviour
         _doc = GetComponent<UIDocument>();
         _menuController = GameObject.Find("UIMenu").GetComponent<MenuController>();
 
-        // Start Menu UI
+        // Labels
+        _clockLabel = _doc.rootVisualElement.Q<Label>("Clock");
+        _clockLabel.text = "00:00";
+
+        // Buttons
         _menuButton = _doc.rootVisualElement.Q<Button>("MenuButton");
         _menuButton.clicked += MenuButtonOnClicked;
+
+        _freeCamButton = _doc.rootVisualElement.Q<Button>("FreeCam");
+        _freeCamButton.clicked += FreeCamButtonOnClicked;
+
+        _twoDButton = _doc.rootVisualElement.Q<Button>("2D");
+        _twoDButton.clicked += TwoDButtonOnClicked;
+
+        _fpvButton = _doc.rootVisualElement.Q<Button>("FPV");
+        _fpvButton.clicked += FPVButtonOnClicked;
 
         _doc.rootVisualElement.visible = false;
         
     }
-    
+
     private void MenuButtonOnClicked()
     {
         _doc.rootVisualElement.visible = false;
         _menuController.Enable();
+    }
+
+    private void FreeCamButtonOnClicked()
+    {
+        Debug.Log("FreeCam");
+    }
+
+    private void TwoDButtonOnClicked()
+    {
+        Debug.Log("2D");
+    }
+
+    private void FPVButtonOnClicked()
+    {
+        Debug.Log("FPV");
+    }
+
+    void Update()
+    {
+        _clockLabel.text = System.DateTime.Now.ToString("HH:mm");
     }
 
     public void Enable()
