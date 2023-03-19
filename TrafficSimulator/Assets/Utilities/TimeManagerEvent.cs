d590bb96c8e04fb8165c3011ee5ad6adfcb9dff1
marc7s <marcus.schagerberg@gmail.com>
2023-03-20 00:37:03 +0100

    Code standard fixes

--- TrafficSimulator/Assets/Utilities/TimeManagerEvent.cs
+++ TrafficSimulator/Assets/Utilities/TimeManagerEvent.cs
@@ -1,29 +1,29 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class TimeManagerEvent : System.IComparable<TimeManagerEvent>, System.IEquatable<TimeManagerEvent>
+namespace Simulation
 {
-    private TimeManagerEvent _prev;
-    private TimeManagerEvent _next;
-    public string TimeStamp;
-    private int _priority;
-
-    public TimeManagerEvent(int priority, string timeStamp) : this(priority, null, null, timeStamp){}
-    public TimeManagerEvent(int priority, TimeManagerEvent prev, TimeManagerEvent next, string timeStamp)
+    public class TimeManagerEvent : System.IComparable<TimeManagerEvent>, System.IEquatable<TimeManagerEvent>
     {
-        _priority = priority;
-        _prev = prev;
-        _next = next;
-        TimeStamp = timeStamp;
-    } 
+        private TimeManagerEvent _prev;
+        private TimeManagerEvent _next;
+        public string TimeStamp;
+        private int _priority;
 
-    public int CompareTo(TimeManagerEvent other)
-    {
-        return _priority.CompareTo(other._priority);
-    }
+        public TimeManagerEvent(int priority, string timeStamp) : this(priority, null, null, timeStamp){}
+        public TimeManagerEvent(int priority, TimeManagerEvent prev, TimeManagerEvent next, string timeStamp)
+        {
+            _priority = priority;
+            _prev = prev;
+            _next = next;
+            TimeStamp = timeStamp;
+        } 
 
-    public bool Equals(TimeManagerEvent other) {
-        return TimeStamp.ToString() == other.TimeStamp.ToString();
+        public int CompareTo(TimeManagerEvent other)
+        {
+            return _priority.CompareTo(other._priority);
+        }
+
+        public bool Equals(TimeManagerEvent other) {
+            return TimeStamp.ToString() == other.TimeStamp.ToString();
+        }
     }
 }
+
