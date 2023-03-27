09861a12cc7a161d48eec62b931d9e1d043c52e4
Felix J <felix.jonsson1993@gmail.com>
2023-03-28 00:08:42 +0200

    Set up code before pull request

---
+++ TrafficSimulator/Assets/User/CarSelectable.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+
+namespace User
+{
+    /// <summary>
+    ///     Communication interface to allow a car to be selected by the user. 
+    /// </summary>
+    public class CarSelectable : Selectable
+    {
+        private Outline _selectOutline;
+        [SerializeField] private Transform _firstPersonPivot;
+
+        private void Start()
+        {
+            _selectOutline = GetComponentInChildren<Outline>();
+            OutlineIsActive(false);
+        }
+
+        public override void Select()
+        {
+            OutlineIsActive(true);
+        }
+
+        public override void Deselect()
+        {
+            OutlineIsActive(false);
+        }
+
+        private void OutlineIsActive(bool isActive)
+        {
+            _selectOutline.enabled = isActive;
+        } 
+        
+        public Transform GetFirstPersonPivot()
+        {
+            return _firstPersonPivot;
+        }
+    }
+}
