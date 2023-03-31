118ba7855aef163ecdae6668b0c5ec67cc32e918
Martin Blom <martinbl@net.chalmers.se>
2023-03-31 21:51:55 +0200

    Original scene file and rq changes

--- TrafficSimulator/Assets/UI/Statistics/Stats/StatsController.cs
+++ TrafficSimulator/Assets/UI/Statistics/Stats/StatsController.cs
@@ -8,16 +8,14 @@ public class StatsController : MonoBehaviour
     private UIDocument _doc;
 
     // Labels
-    private Label _totalTimeOnRoad;
-    private Label _totalDistanceTraveled;
+    private Label _fuelConsumption;
+    private Label _timeInTraffic;
 
     private void Awake()
     {
         _doc = GetComponent<UIDocument>();
 
-        // Labels
-        //_totalTimeOnRoad = _doc.rootVisualElement.Q<Label>("TotalTimeOnRoad");
-        //_totalDistanceTraveled = _doc.rootVisualElement.Q<Label>("TotalDistanceTraveled");
+
         
     }
     
@@ -28,13 +26,11 @@ public class StatsController : MonoBehaviour
             ResetInfo();
             return;
         }
-        //_totalTimeOnRoad.text = car.
-        //_totalDistanceTraveled.text = car.TotalDistance.ToString("0.00");
+
     }
 
     public void ResetInfo()
     {
-        //_totalTimeOnRoad.text = "N/A";
-        //_totalDistanceTraveled.text = "N/A";
+
     }
 }
