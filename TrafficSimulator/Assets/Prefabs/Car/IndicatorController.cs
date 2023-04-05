177bcfcb39ae3c0c76b46168c533937d0b3c9fa7
Hannes Kaulio <kaulio@student.chalmers.se>
2023-04-05 15:14:40 +0200

    Code review fix

--- TrafficSimulator/Assets/Prefabs/Car/IndicatorController.cs
+++ TrafficSimulator/Assets/Prefabs/Car/IndicatorController.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using DataModel;
 
@@ -10,40 +8,53 @@ namespace Car
         Right,
         Left,
         Off,
-        Warning
+        Hazard
     }
-public class IndicatorController : MonoBehaviour
-{
-    [SerializeField] private Light[] _leftIndicatorLights;
-    [SerializeField] private Light[] _rightIndicatorLights;
-    [SerializeField] private float _intervalTime = 0.5f;
-    private float _lastSwitchTime = 0f;
-    private IndicatorState _indicatorState = IndicatorState.Off;
-    private bool _isLightOn;
-
-    public void SetIndicator(TurnDirection turnDirection)
+    public class IndicatorController : MonoBehaviour
     {
-        IndicatorState newIndicatorState = turnDirection == TurnDirection.Left ? IndicatorState.Left : turnDirection == TurnDirection.Right ? IndicatorState.Right : IndicatorState.Off;
-        if (newIndicatorState == _indicatorState)
-            return;
-        _indicatorState = newIndicatorState;
-        _lastSwitchTime = Time.time;
-    }
+        [SerializeField] private Light[] _leftIndicatorLights;
+        [SerializeField] private Light[] _rightIndicatorLights;
+        [SerializeField] private float _intervalTime = 0.5f;
+        private float _lastSwitchTime = 0f;
+        private IndicatorState _indicatorState = IndicatorState.Off;
+        private bool _isLightOn;
 
-    void Update()
-    {
-        if (Time.time - _lastSwitchTime > _intervalTime)
+        public void SetIndicator(IndicatorState indicatorState)
         {
-            _isLightOn = !_isLightOn;
+            if (indicatorState == _indicatorState)
+                return;
+            _indicatorState = indicatorState;
             _lastSwitchTime = Time.time;
-            bool turnOnLeftIndicators = _isLightOn && (_indicatorState == IndicatorState.Left || _indicatorState == IndicatorState.Warning);
-            bool turnOnRightIndicators = _isLightOn && (_indicatorState == IndicatorState.Right || _indicatorState == IndicatorState.Warning);
+        }
 
-            foreach (Light leftIndicatorLight in _leftIndicatorLights)
-                leftIndicatorLight.enabled = turnOnLeftIndicators;
-            foreach (Light rightIndicatorLight in _rightIndicatorLights)
-                rightIndicatorLight.enabled = turnOnRightIndicators;
+        void Update()
+        {
+            if (Time.time - _lastSwitchTime > _intervalTime)
+            {
+                _isLightOn = !_isLightOn;
+                _lastSwitchTime = Time.time;
+                bool turnOnLeftIndicators = _isLightOn && (_indicatorState == IndicatorState.Left || _indicatorState == IndicatorState.Hazard);
+                bool turnOnRightIndicators = _isLightOn && (_indicatorState == IndicatorState.Right || _indicatorState == IndicatorState.Hazard);
+
+                foreach (Light leftIndicatorLight in _leftIndicatorLights)
+                    leftIndicatorLight.enabled = turnOnLeftIndicators;
+                foreach (Light rightIndicatorLight in _rightIndicatorLights)
+                    rightIndicatorLight.enabled = turnOnRightIndicators;
+            }
+        }
+        public static IndicatorState TurnDirectionToIndicatorState(TurnDirection turnDirection)
+        {
+            switch (turnDirection)
+            {
+                case TurnDirection.Left:
+                    return IndicatorState.Left;
+                case TurnDirection.Right:
+                    return IndicatorState.Right;
+                case TurnDirection.Straight:
+                    return IndicatorState.Off;
+                default:
+                    return IndicatorState.Off;
+            }
         }
     }
-}
 }
