1f108ec025b535d33a1f438264ce9dca39bbe7f5
Felix J <felix.jonsson1993@gmail.com>
2023-04-15 19:46:16 +0200

    Connected camera button style sheet

---
+++ TrafficSimulator/Assets/UI/NonFocusableButton.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine.UIElements;
+
+namespace UI
+{
+    public class NonFocusableButton : Button
+    {
+        public NonFocusableButton()
+        {
+            focusable = false;
+        }
+    }
+}
--- TrafficSimulator/Assets/UI/OverlayController.cs
+++ TrafficSimulator/Assets/UI/OverlayController.cs
@@ -1,4 +1,3 @@
-using System;
 using Cam;
 using UnityEngine;
 using UnityEngine.UIElements;
@@ -18,11 +17,13 @@ namespace UI
         // Statistics UI
         private VisualElement _statisticsUI;
         public bool _isStatisticsOpen = false;
-
+        
         // Camera Buttons
+        private StyleSheet _cameraButtonStyles;
         private Button _defaultCameraButton;
         private Button _focusedCameraButton;
         private Button _fpvButton;
+        private Button _currentlyHighlightedCameraButton;
 
         // Mode Buttons
         private Button _statisticsButton;
@@ -43,7 +44,8 @@ namespace UI
         void Awake()
         {
             _doc = GetComponent<UIDocument>();
-
+            _cameraButtonStyles = Resources.Load<StyleSheet>("CameraButtonStyle");
+            _doc.rootVisualElement.styleSheets.Add(_cameraButtonStyles);
             //_menuController = GameObject.Find("UIMenu").GetComponent<MenuController>();
 
             // Labels
@@ -57,13 +59,16 @@ namespace UI
             _menuButton.clicked += MenuButtonOnClicked;
 
             _defaultCameraButton = _doc.rootVisualElement.Q<Button>("Default");
+            _defaultCameraButton.AddToClassList("button");
             _defaultCameraButton.clicked += DefaultCameraButtonClicked;
 
             _focusedCameraButton = _doc.rootVisualElement.Q<Button>("Focus");
+            _focusedCameraButton.AddToClassList("button");
             _focusedCameraButton.clicked += FocusedCameraButtonOnClicked;
             GreyOutButton(_focusedCameraButton);
 
             _fpvButton = _doc.rootVisualElement.Q<Button>("FPV");
+            _fpvButton.AddToClassList("button");
             _fpvButton.clicked += FPVButtonOnClicked;
             GreyOutButton(_fpvButton);
 
@@ -108,6 +113,30 @@ namespace UI
                     GreyOutButton(_focusedCameraButton);
                 }
             };
+            _cameraManager.OnCameraChanged += type =>
+            {
+                // Reset all camera buttons to normal state
+                RemoveCameraButtonHighlight(_defaultCameraButton);
+                RemoveCameraButtonHighlight(_focusedCameraButton);
+                RemoveCameraButtonHighlight(_fpvButton);
+
+                // Highlight the correct camera button based on the camera type
+                switch (type)
+                {
+                    case CameraManager.CustomCameraType.Default:
+                        HighlightCameraButton(_defaultCameraButton);
+                        break;
+                    case CameraManager.CustomCameraType.Focus:
+                        HighlightCameraButton(_focusedCameraButton);
+                        break;
+                    case CameraManager.CustomCameraType.FirstPersonDriver:
+                        HighlightCameraButton(_fpvButton);
+                        break;
+                    default:
+                        Debug.LogWarning("Unknown camera type.");
+                        break;
+                }
+            };
         }
         
         private void FindCameraManager()
@@ -119,19 +148,41 @@ namespace UI
             }
         }
         
-        public void GreyOutButton(Button button)
+        private void GreyOutButton(Button button)
         {
             button.style.backgroundColor = new Color(0.5f, 0.5f, 0.5f, 1.0f);
             button.style.color = new Color(0.2f, 0.2f, 0.2f, 1.0f);
             button.SetEnabled(false);
         }
-        
-        public void RestoreButton(Button button)
+
+        private void RestoreButton(Button button)
         {
             button.style.backgroundColor = StyleKeyword.Null;
             button.style.color = StyleKeyword.Null;
             button.SetEnabled(true);
         }
+
+        private void HighlightCameraButton(Button buttonToHighlight)
+        {
+            if (_currentlyHighlightedCameraButton != null)
+            {
+                RemoveCameraButtonHighlight(_currentlyHighlightedCameraButton);
+            }
+
+            AddButtonHighlight(buttonToHighlight);
+            _currentlyHighlightedCameraButton = buttonToHighlight;
+        }
+
+        private void AddButtonHighlight(Button button)
+        {
+            button.AddToClassList("button-highlighted");
+        }
+
+        private void RemoveCameraButtonHighlight(Button button)
+        {
+            button.RemoveFromClassList("button-highlighted");
+        }
+
 
         private void MenuButtonOnClicked()
         {
@@ -147,7 +198,7 @@ namespace UI
 
         private void FocusedCameraButtonOnClicked()
         {
-            _cameraManager.ToggleThirdPersonCamera();
+            _cameraManager.ToggleFocusCamera();
         }
 
         private void FPVButtonOnClicked()
