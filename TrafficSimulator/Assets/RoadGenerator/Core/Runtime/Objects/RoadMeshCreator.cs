f168a35a95b1c169cbd05d29dd63638b5f2c50a7
Hannes Kaulio <kaulio@student.chalmers.se>
2023-04-29 20:29:12 +0200

    Added one way road material

--- TrafficSimulator/Assets/RoadGenerator/Core/Runtime/Objects/RoadMeshCreator.cs
+++ TrafficSimulator/Assets/RoadGenerator/Core/Runtime/Objects/RoadMeshCreator.cs
@@ -10,6 +10,7 @@ namespace RoadGenerator
 
         [Header ("Material settings")]
         [SerializeField] private Material _laneMaterial;
+        [SerializeField] private Material _oneWayRoadLaneMaterial;
         [SerializeField] private Material _bottomMaterial;
         [SerializeField] private float _textureTilingScale = 100;
         
@@ -44,7 +45,11 @@ namespace RoadGenerator
             // This makes the material independent from other roads
             if((_laneMaterialCopy == null && _laneMaterial != null) || (_laneMaterialCopy != null && _laneMaterial != null && _laneMaterialCopy.name != _laneMaterial.name))
             {
-                _laneMaterialCopy = new Material(_laneMaterial);
+                _laneMaterialCopy = new Material(_road.IsOneWay ? _oneWayRoadLaneMaterial : _laneMaterial);
+            }
+            else if (_road.IsOneWay)
+            {
+                _laneMaterialCopy = new Material(_road.IsOneWay ? _oneWayRoadLaneMaterial : _laneMaterial);
             }
                 
             
