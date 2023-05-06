584ee60af67f817b03d281c9872f4295ea76178f
marc7s <marcus.schagerberg@gmail.com>
2023-05-06 12:37:28 +0200

    Updated parking spot generation

--- TrafficSimulator/Assets/RoadGenerator/Core/Runtime/DataModel/POIs/Parking.cs
+++ TrafficSimulator/Assets/RoadGenerator/Core/Runtime/DataModel/POIs/Parking.cs
@@ -25,7 +25,7 @@ namespace POIs
         private List<POINode> _parkingSpots = new List<POINode>();
         
         [SerializeField][HideInInspector] protected GameObject _parkNodeContainer;
-        private const float PARKING_SIZE = 5f;
+        [HideInInspector] protected Vector2 _parkingSize = new Vector2(4f, 6f);
         private const string PARK_NODE_CONTAINER_NAME = "Park Nodes";
         private const string PARK_NODE_NAME = "ParkNode";
         
@@ -46,22 +46,30 @@ namespace POIs
         private void GenerateParkingSpots()
         {
             _parkingSpots.Clear();
-            float sideOffset = Size.z / 2;
-            Vector3 startPos = transform.position + transform.right * sideOffset;
+            float forwardOffset = Size.z / 2;
+            Vector3 startPos = transform.position - transform.right * forwardOffset;
             
+            int parkingSpotsPerSide = Mathf.FloorToInt(Size.x / _parkingSize.x);
+            float sideOffsetDelta = Size.x / parkingSpotsPerSide;
+
             for(int side = 0; side < 2; side++)
             {
-                for(float i = PARKING_SIZE / 2; i < Size.z; i += PARKING_SIZE)
+                float sideOffset = _parkingSize.x / 2;
+                for(int i = 0; i < parkingSpotsPerSide; i++, sideOffset += sideOffsetDelta)
                 {
                     int sideCoef = side == 0 ? -1 : 1;
-                    Vector3 sideOffsetVector = transform.forward * (sideOffset + sideCoef * PARKING_SIZE / 2);
-                    Vector3 forwardOffsetVector = -transform.right * Size.x + transform.right * i;
+                    
+                    // The amount to offset from the rear edge
+                    Vector3 forwardOffsetVector = transform.forward * (forwardOffset + sideCoef * _parkingSize.y / 2);
+                    
+                    // The amount to offset from the right edge
+                    Vector3 sideOffsetVector = transform.right * sideOffset;
                     
                     Quaternion rotation = transform.rotation * (side == 0 ? Quaternion.Euler(Vector3.up * 180) : Quaternion.identity);
-                    POINode parkingSpot = new POINode(startPos + sideOffsetVector + forwardOffsetVector, rotation);
+                    POINode parkingSpot = new POINode(startPos + forwardOffsetVector + sideOffsetVector, rotation);
                     _parkingSpots.Add(parkingSpot);
                 }
-                sideOffset *= -1;
+                forwardOffset *= -1;
             }
         }
 
