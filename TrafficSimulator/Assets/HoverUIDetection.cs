fd89befe285a2043d010c76059724abcdff84f9f
Felix J <felix.jonsson1993@gmail.com>
2023-05-12 13:07:38 +0200

    PR Changes

--- TrafficSimulator/Assets/HoverUIDetection.cs
+++ TrafficSimulator/Assets/HoverUIDetection.cs
@@ -18,11 +18,10 @@ public class HoverUIDetection : MonoBehaviour
     void Update()
     {
         _pointerEventData.position = Mouse.current.position.ReadValue();
-        var results = new List<RaycastResult>();
+        List<RaycastResult> results = new List<RaycastResult>();
         _eventSystem.RaycastAll(_pointerEventData, results);
 
         bool isHoveringUIGO = results.Exists(result => LayerMask.LayerToName(result.gameObject.layer) == "UI");
         UserSelectManager.Instance.IsHoveringUIElement = isHoveringUIGO;
-
     }
 }
