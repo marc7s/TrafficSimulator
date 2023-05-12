9157a9c9a34949aae4b14246b04341b024721faf
Martin Blom <martinbl@net.chalmers.se>
2023-05-12 15:36:44 +0200

    Deleted unused code & improved _showFPS

--- TrafficSimulator/Assets/UI/OverlayController.cs
+++ TrafficSimulator/Assets/UI/OverlayController.cs
@@ -14,25 +14,12 @@ namespace UI
         // Overlay Button
         private Button _menuButton;
 
-        // Car statistics UI
-        private VisualElement _statisticsUI;
-        public bool _isStatisticsOpen = false;
-
-        // World statistics UI
-        private VisualElement _worldUI;
-        public bool _isWorldOpen = false;
-
         // Camera Buttons
         private StyleSheet _cameraButtonStyles;
         private Button _defaultCameraButton;
         private Button _focusedCameraButton;
         private Button _fpvButton;
         private Button _currentlyHighlightedCameraButton;
-
-        // Mode Buttons
-        private Button _statisticsButton;
-        private Button _worldOptionButton;
-        private Button _editorButton;
 
         // Clock Buttons
         private Button _rewindButton;
@@ -45,6 +32,7 @@ namespace UI
         // Labels
         private Label _clockLabel;
         private Label _fpsLabel;
+        private bool _showFPS = false;
 
         private const int _fpsUpdateFrequency = 15;
         private float _fpsLastUpdateTime = 0;
@@ -83,34 +71,7 @@ namespace UI
             _fpvButton.AddToClassList("button");
             _fpvButton.clicked += FPVButtonOnClicked;
             GreyOutButton(_fpvButton);
-
             
-            /*_statisticsButton = _doc.rootVisualElement.Q<Button>("Statistics");
-            _statisticsButton.clicked += StatisticsButtonOnClicked;
-
-            // Get car statistics UI visual element
-            _statisticsUI = _doc.rootVisualElement.Q<VisualElement>("StatisticsWindow");
-            _statisticsUI.pickingMode = PickingMode.Position;
-            _statisticsUI.visible = false;
-            // Make draggable
-            _statisticsUI.AddManipulator(new DragManipulator());
-            _statisticsUI.RegisterCallback<DropEvent>(evt => Debug.Log($"{evt.target} dropped on {evt.droppable}"));
-
-            // Get world statistics UI visual element
-            _worldUI = _doc.rootVisualElement.Q<VisualElement>("WorldWindow");
-            _worldUI.pickingMode = PickingMode.Position;
-            _worldUI.visible = false;
-            // Make draggable
-            _worldUI.AddManipulator(new DragManipulator());
-            _worldUI.RegisterCallback<DropEvent>(evt => Debug.Log($"{evt.target} dropped on {evt.droppable}"));
-
-            _worldOptionButton = _doc.rootVisualElement.Q<Button>("WorldOptions");
-            _worldOptionButton.clicked += WorldOptionButtonOnClicked;
-
-            _editorButton = _doc.rootVisualElement.Q<Button>("Editor");
-            _editorButton.clicked += EditorButtonOnClicked;*/
-            
-
             _rewindButton = _doc.rootVisualElement.Q<Button>("Rewind");
             _rewindButton.clicked += RewindButtonOnClicked;
 
@@ -123,13 +84,15 @@ namespace UI
             _fastForwardButton = _doc.rootVisualElement.Q<Button>("Fastforward");
             _fastForwardButton.clicked += FastForwardButtonOnClicked;
 
-            _doc.rootVisualElement.visible = false;
+            // Load settings
+            _showFPS = PlayerPrefsGetBool(FPS_COUNTER);
         }
 
         private void Start()
         {
             // Set FPS visibility
             _fpsLabel.visible = PlayerPrefsGetBool(FPS_COUNTER);
+
             UserSelectManager.Instance.OnSelectedGameObject += selectedGameObject =>
             {
                 if (selectedGameObject)
@@ -216,8 +179,6 @@ namespace UI
 
         private void MenuButtonOnClicked()
         {
-            _statisticsUI.visible = false;
-            _worldUI.visible = false;
             _doc.rootVisualElement.visible = false;
             SceneManager.LoadScene((SceneManager.GetActiveScene().buildIndex - 1),  LoadSceneMode.Single);
         }
@@ -235,28 +196,6 @@ namespace UI
         private void FPVButtonOnClicked()
         {
             _cameraManager.ToggleFirstPersonDriverCamera();
-        }
-
-        private void StatisticsButtonOnClicked()
-        {
-            _isStatisticsOpen = !_isStatisticsOpen;
-            _statisticsUI.visible = _isStatisticsOpen;
-            
-        }
-
-        public GameObject WorldWindowStatistic;        
-        
-        private void WorldOptionButtonOnClicked()
-        {
-            _isWorldOpen = !_isWorldOpen;
-            _worldUI.visible = _isWorldOpen;
-            WorldWindowStatistic.SetActive(_isWorldOpen);
-            
-        }
-
-        private void EditorButtonOnClicked()
-        {
-            Debug.Log("Editor");
         }
 
         private void RewindButtonOnClicked()
@@ -281,7 +220,7 @@ namespace UI
         {
             _clockLabel.text = TimeManager.Instance.Timestamp;
             // FPS counter
-            if(PlayerPrefsGetBool(FPS_COUNTER) && Time.time >= _fpsLastUpdateTime + 1f / _fpsUpdateFrequency)
+            if(_showFPS && Time.time >= _fpsLastUpdateTime + 1f / _fpsUpdateFrequency)
                 DisplayFPS(1f / Time.unscaledDeltaTime);
         }
 
@@ -295,11 +234,6 @@ namespace UI
         private bool PlayerPrefsGetBool(string name)
         {
             return PlayerPrefs.GetInt(name, 0) == 1;
-        }
-
-        public void Enable()
-        {
-            _doc.rootVisualElement.visible = true;
         }
     }
 }
