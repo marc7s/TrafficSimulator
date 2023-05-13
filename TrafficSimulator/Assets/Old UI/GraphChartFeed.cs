823dcfca8d9abb5bffa7f8d6db029903d00e467a
Felix J <felix.jonsson1993@gmail.com>
2023-05-13 10:04:59 +0200

    Graph work

--- TrafficSimulator/Assets/Old UI/GraphChartFeed.cs
+++ TrafficSimulator/Assets/Old UI/GraphChartFeed.cs
@@ -1,33 +1,63 @@
-using UnityEngine;
 using ChartAndGraph;
-using System.Collections;
+using UnityEngine;
 
-public class GraphChartFeed : MonoBehaviour
+namespace Old_UI
 {
-    private GraphChartBase _graph;
-    private WorldDataGatherer _worldDataGatherer;
-
-    private void Start()
+    public class GraphChartFeed : MonoBehaviour
     {
-        _worldDataGatherer = GameObject.Find("RoadSystem").GetComponent<WorldDataGatherer>();
-        _graph = GetComponent<GraphChartBase>();
+        private GraphChartBase _graph;
+        private WorldDataGatherer _worldDataGatherer;
+
+        private void Start()
+        {
+            _worldDataGatherer = GameObject.Find("RoadSystem").GetComponent<WorldDataGatherer>();
+            _graph = GetComponent<GraphChartBase>();
         
         
-        if (_graph != null)
-        {
-            _graph.Scrollable = false;
-            LoadAllTimeGraph();
+            if (_graph != null)
+            {
+                _graph.Scrollable = false;
+                LoadAllTimeGraph();
+            }
         }
-    }
 
-    public void LoadAllTimeGraph()
-    {
-        _graph.DataSource.StartBatch();
-        _graph.DataSource.ClearCategory("Emission");
-        for (int i = 0; i < _worldDataGatherer.TotalSecondsElapsed; i++)
+        public void LoadAllTimeGraph()
         {
-            _graph.DataSource.AddPointToCategory("Emission", i, _worldDataGatherer.FuelConsumedPerSecondHistory[i]);
+            _graph.DataSource.StartBatch();
+            _graph.DataSource.ClearCategory("Emission");
+            for (int i = 0; i < _worldDataGatherer.TotalSecondsElapsed; i++)
+            {
+                _graph.DataSource.AddPointToCategory("Emission", i, _worldDataGatherer.FuelConsumedPerSecondHistory[i]);
+            }
+            _graph.DataSource.EndBatch();
         }
-        _graph.DataSource.EndBatch();
+    
+        public void LoadLast3MinGraph()
+        {
+            _graph.DataSource.StartBatch();
+            _graph.DataSource.ClearCategory("Emission");
+
+            float timeToUse = 180 > _worldDataGatherer.TotalSecondsElapsed ? _worldDataGatherer.TotalSecondsElapsed : 180;
+        
+            for (int i = 0; i < timeToUse; i++)
+            {
+                _graph.DataSource.AddPointToCategory("Emission", i, _worldDataGatherer.FuelConsumedPerSecondHistory[i]);
+            }
+            _graph.DataSource.EndBatch();
+        }
+
+        public void LoadLast30SecondsGraph()
+        {
+            _graph.DataSource.StartBatch();
+            _graph.DataSource.ClearCategory("Emission");
+        
+            float timeToUse = 30 > _worldDataGatherer.TotalSecondsElapsed ? _worldDataGatherer.TotalSecondsElapsed : 30;
+        
+            for (int i = 0; i < timeToUse; i++)
+            {
+                _graph.DataSource.AddPointToCategory("Emission", i, _worldDataGatherer.FuelConsumedPerSecondHistory[i]);
+            }
+            _graph.DataSource.EndBatch();
+        }
     }
 }
