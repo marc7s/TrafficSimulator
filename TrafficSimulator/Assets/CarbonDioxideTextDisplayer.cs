1d5aa6c732b76b8313811777a6473333a35a83a1
Felix J <felix.jonsson1993@gmail.com>
2023-05-13 22:34:30 +0200

    Display CO2

--- TrafficSimulator/Assets/CarbonDioxideTextDisplayer.cs
+++ TrafficSimulator/Assets/CarbonDioxideTextDisplayer.cs
@@ -4,26 +4,33 @@ using UnityEngine;
 public class CarbonDioxideTextDisplayer : MonoBehaviour
 {
     private const string DefaultText = "Estimated CO<sup>2</sup>: ";
+    private const string Units = " kg";
     private TextMeshProUGUI _carbonDioxideEstimateText;
     private WorldDataGatherer _worldDataGatherer;
 
     private void Start()
     {
+        _carbonDioxideEstimateText = GetComponent<TextMeshProUGUI>();
         _worldDataGatherer = GameObject.Find("RoadSystem").GetComponent<WorldDataGatherer>();
+    }
+
+    private void DisplayCO2(float co2Amount)
+    {
+        _carbonDioxideEstimateText.text = DefaultText + co2Amount.ToString("0.0000") + Units;
     }
 
     public void DisplayAllTime()
     {
-        _carbonDioxideEstimateText.text = DefaultText + _worldDataGatherer.Co2EmissionsAllTime.ToString("0.0000");
+        DisplayCO2(_worldDataGatherer.Co2EmissionsAllTime);
     }
 
     public void Display3Min()
     {
-        _carbonDioxideEstimateText.text = DefaultText + _worldDataGatherer.Co2EmissionsAllTime.ToString("0.0000");
+        DisplayCO2(_worldDataGatherer.Co2EmissionsLast3Min);
     }
 
     public void Display30Seconds()
     {
-        _carbonDioxideEstimateText.text = DefaultText + _worldDataGatherer.Co2EmissionsAllTime.ToString("0.0000");
+        DisplayCO2(_worldDataGatherer.Co2EmissionsLast30Sec);
     }
 }
