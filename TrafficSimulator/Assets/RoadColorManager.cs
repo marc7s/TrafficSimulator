89490f959f6e9271de8358fef0f8bbad8220104a
Felix J <felix.jonsson1993@gmail.com>
2023-05-16 11:19:26 +0200

    Fix roadcolor toggle

--- TrafficSimulator/Assets/RoadColorManager.cs
+++ TrafficSimulator/Assets/RoadColorManager.cs
@@ -7,21 +7,25 @@ public class RoadColorManager : MonoBehaviour
 {
     
     [SerializeField] private GameObject _roadsParent;
-    private List<GameObject> _roads;
+    private List<GameObject> _roads = new List<GameObject>();
+    [SerializeField] private bool _isEnabled = false;
 
     void Start()
     {
         foreach(Transform child in _roadsParent.transform)
         {
+            print(child.gameObject);
             _roads.Add(child.gameObject);
         }
     }
     
     public void SetRoadColor()
     {
+        _isEnabled = !_isEnabled;
         foreach (GameObject road in _roads)
         {
-            road.GetComponent<EmissionColor>().EmissionEnabled = true;
+            print("manager" + _isEnabled);
+            road.GetComponent<EmissionColor>().EmissionEnabled = _isEnabled;
         }
     }
 }
